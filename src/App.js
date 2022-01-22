98ad8fb663ab5e034acee046b7d14fba3eea2182
Bfatima25 <bushrafatima102@gmail.com>
2022-01-21 19:51:03 -0600

    Added components to .src Folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import FormToDo from "./Components/FormToDo";
+import ToDoList from "./Components/ToDoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todolist, setTodolist] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="ToDoList-app">
+      <header>
+        <h1>What's The Plan Today?</h1>
       </header>
+      <FormToDo 
+        inputText={inputText} 
+        todolist={todolist} 
+        setTodolist={setTodolist} 
+        setInputText={setInputText} />
+      <ToDoList setTodolist={setTodolist} todolist={todolist} />
     </div>
   );
 }
